7c5e82735a0cc333ac59191c8ccc3c4d2d3d4cb0
rezwan <rezwan@Xubuntu.myguest.virtualbox.org>
2024-07-05 23:21:05 -0400

    Display digits and operators on the screen

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 "use strict"
+
+// -------------------Fucntions for basic operation---------------------
 function add(a, b) {
   return a + b;
 } 
@@ -15,8 +17,8 @@ function divide(a, b) {
   return a / b;
 } 
 
-function operate(x, operator, y) {
-  switch(operator) {
+function operate(x, op, y) {
+  switch(op) {
     case '+': return add(x, y);
     case '-': return subtract(x, y);
     case '*': return multiply(x, y);
@@ -24,3 +26,33 @@ function operate(x, operator, y) {
   }
 }
 
+let operator = null;
+let num1 = null;
+let num2 = null;
+
+const display = document.querySelector('h1');
+const digitButtons = document.querySelectorAll('button.digit');
+const operatorButtons = document.querySelectorAll('button.operator')
+digitButtons.forEach(button => button.addEventListener('click', displayDigit));
+operatorButtons.forEach(button => button.addEventListener('click', displayOperator));
+
+function displayDigit(event) {
+  let element = event.target;
+  if(isDisplayEmpty()) 
+    display.textContent = element.textContent;
+  else {
+    display.textContent += element.textContent;
+  }
+}
+
+function displayOperator(event) {
+  let element = event.target;
+  display.textContent += ` ${element.textContent} `; 
+  operator = element.textContent;
+  console.log(operator);
+}
+
+// Helper Functions
+function isDisplayEmpty() {
+  return display.textContent === "Calculator";
+}
