2a97669beec09cf1eec375de953c0b41b0d7d1df
rezwan <rezwan@Xubuntu.myguest.virtualbox.org>
2024-07-14 03:15:01 -0400

    Add keyboard support

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Fuctions used for calculation
 function add(a, b) {
   return round(a + b);
-  
+
 }
 
 function subtract(a, b) {
@@ -20,7 +20,7 @@ function divide(a, b) {
 
 // Round foating point numbers
 function round(result) {
-  if(!Number.isInteger(result)) {
+  if (!Number.isInteger(result)) {
     return +result.toFixed(2)
   }
   return result;
@@ -92,7 +92,7 @@ function handleOperatorClick(event) {
 
 function isOperatorReady() {
   // Not ready for operator if number is NaN or we already have an operator
-  if (Number.isNaN(+num1) || operator !== '') {
+  if (num1.length === 0 || Number.isNaN(+num1) || operator !== '') {
     return false;
   }
   return true;
@@ -100,7 +100,7 @@ function isOperatorReady() {
 
 // On minus button clikc check to see if it's to make a number negative
 function makeNumberNegative(symbol) {
-  if (symbol === '−') {
+  if (symbol === '−' || symbol === '-') {
     if (num1 === '') {
       num1 = '-';
       mainDisplay.textContent += num1;
@@ -154,26 +154,90 @@ function isValidNumber(number) {
 const clearButton = document.querySelector('.button.clear');
 const deleteButton = document.querySelector('.button.delete');
 
-clearButton.addEventListener('click', () => {
+clearButton.addEventListener('click', clear);
+
+function clear() {
   mainDisplay.textContent = '';
   subDisplay.textContent = '';
   num1 = num2 = operator = '';
-});
-
+}
 
 deleteButton.addEventListener('click', handleDelete);
 
 function handleDelete() {
 
-  if(operator === '') {
+  if (operator === '') {
     mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
     num1 = mainDisplay.textContent;
-  } else if(operator !== '' && num2 === '') {
+  } else if (operator !== '' && num2 === '') {
     mainDisplay.textContent = mainDisplay.textContent.slice(0, -3);
     operator = '';
   } else {
     mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
-   num2 = mainDisplay.textContent.split(operator)[1].trim();
+    num2 = mainDisplay.textContent.split(operator)[1].trim();
   }
 }
 
+// --------------------------Keyborad Support --------------------
+document.addEventListener('keydown', handleKeyPress);
+function handleKeyPress(event) {
+  let key = event.key;
+  if (key >= '0' && key <= '9') {
+    if (operator === '') {
+      num1 += key;
+      mainDisplay.textContent = num1;
+    } else {
+      num2 += key;
+      // cant append num2 directly to mainDisplay, since it will append duplcate digits
+      // so insted extract previous num2 from mainDisplay and replace it with the new one
+      if (num2.length > 1) {
+        let numbers = mainDisplay.textContent.split(operator); // Split the two numbers
+        numbers[1] = ` ${num2}`;  // Change the second number to the new num2
+        let newDisplyText = numbers.join(operator); // Create the new text
+        mainDisplay.textContent = newDisplyText; // Display the new text
+      } else {
+        mainDisplay.textContent += num2;
+      }
+      result = operate(+num1, operator, +num2);
+      console.log(result);
+      subDisplay.textContent = result;
+    }
+  }
+  else if (['+', '-', '*', '/'].includes(key)) {
+    // Check to see if we are trying to make a number negative
+    makeNumberNegative(key);
+
+    // Prevnt multiple orperator click
+    if (isOperatorReady()) {
+  
+      switch (key) {
+        case '+':
+          operator = key;
+          break;
+        case '-':
+          operator = `−`;
+          break;
+        case '*':
+          operator = `×`;
+          break;
+        case '/':
+          operator = `÷`;
+          break;
+      }
+      mainDisplay.textContent += ` ${operator} `;
+    }
+  }
+  else if(key === '.') {
+    handleDecimalClick();
+  }
+  else if(key === '=' || key === 'Enter') {
+    handleEqualClick();
+  }
+  else if(key === 'Delete') {
+   clear();
+  }
+  else if(key === 'Backspace') {
+    handleDelete();
+  }
+  console.log(key);
+}
